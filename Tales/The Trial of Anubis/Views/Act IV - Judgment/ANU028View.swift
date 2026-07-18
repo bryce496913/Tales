ab26ae8cfcff6268162ea666b03b67dd54068cb4
bryce496913 <123977944+bryce496913@users.noreply.github.com>
2026-07-18 10:38:19 -0400

    Clean up Trial of Anubis story structure

--- Tales/The Trial of Anubis/Views/Act IV - Judgment/ANU028View.swift
+++
@@ -1,3 +0,0 @@
-import SwiftUI
-// ANU028View is implemented in TrialOfAnubisViews.swift because it performs deterministic ending routing.
-struct ANU028View_Previews: PreviewProvider { static var previews: some View { ANU028View().environmentObject(TrialOfAnubisNavigationState(defaults: UserDefaults(suiteName: "preview-anu-028")!)) } }
